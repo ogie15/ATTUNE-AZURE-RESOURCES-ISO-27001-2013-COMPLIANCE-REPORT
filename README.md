a992405925d12a52e981f83754f4823ce96c6555
Okougbo Ibhadogiemu <iokougbo@gmail.com>
2021-10-17 23:26:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Using Attune to get ISO 27001:2013 Non-Compliance Report for Azure Resources
--- tet.ps1
+++ tet.ps1
@@ -59,3 +59,145 @@ PolicyAssignmentId : /subscriptions/8d82306b-37fe-4d67-bb74-ebf28b32517b/provide
 Properties         : Microsoft.Azure.Commands.ResourceManager.Cmdlets.Implementation.Policy.PsPolicyAssignmentProperties
 
 # ==================================================================================
+
+# ========================SUP===Audit diagnostic setting=================================
+# -or $PolicyDisplayName -match 'Audit diagnostic setting'
+$rev = (Get-AzResource).Name
+
+$AllowedLocations = @{}
+
+$AllowedLocations += @{'listOfResourceTypes'=($rev)}
+
+$pld = Get-AzPolicyDefinition -Name "7f89b1eb-583c-429a-8828-af049802c1d9"
+
+New-AzPolicyAssignment -Name $pld.Name -DisplayName $pld.Properties.DisplayName -Description $pld.Properties.Description -PolicyDefinition $pld -Scope "/subscriptions/$($Subscription.Id)"`
+-PolicyParameterObject $AllowedLocations
+# ========================SUP============Add system-assigned managed identity to enable Guest Configuration assignments on VMs with a user-assigned identity========================
+
+497dff13-db2a-4c0f-8603-28fa3b331ab6
+
+$pld = Get-AzPolicyDefinition -Name "497dff13-db2a-4c0f-8603-28fa3b331ab6"
+
+New-AzPolicyAssignment -Name $pld.Name -DisplayName $pld.Properties.DisplayName -Description $pld.Properties.Description -PolicyDefinition $pld -Scope "/subscriptions/$($Subscription.Id)"`
+-Location 'eastus' -AssignIdentity
+
+$pld.Properties.PolicyRule
+# ========================SUP====================================
+$pld = Get-AzPolicyDefinition -Name "1f905d99-2ab7-462c-a6b0-f709acca6c8f" | Where-Object{$_.Properties.DisplayName -like "Monitor missing Endpoint Protection in Azure Security Center"}
+
+$pld = Get-AzPolicyDefinition | Where-Object{$_.Properties.DisplayName -like "Monitor missing Endpoint Protection in Azure Security Center"}
+
+$pld.Properties
+
+# ==========================
+$Script:AllAZPolicies = Get-AzPolicyDefinition -SubscriptionId $Script:Subscription.Id
+
+$Script:AllComplianceStates = Get-AzPolicyState -Filter "ComplianceState eq 'NonCompliant'" -SubscriptionId $Script:Subscription.Id
+
+$Script:NONPolicies = Get-AzPolicyStateSummary -SubscriptionId $Script:Subscription.Id
+
+$Script:AllComplianceStates | Where-Object{$_.Properties.DisplayName -like "A maximum of 3 owners should be designated for your subscription"; Write-Output $_.Properties.DisplayName}
+
+PolicyDefinitionName     : 34c877ad-507e-4c82-993e-3452a6e0ad3c
+
+Get-AzPolicyState -PolicyDefinitionName "0961003e-5a0a-4549-abde-af6a37f2724d"
+
+Get-AzPolicyState -PolicyDefinitionName "2a1a9cdf-e04d-429a-8416-3bfb72a1b26f"
+
+Get-AzPolicyState -PolicyDefinitionName "3cf2ab00-13f1-4d0c-8971-2ac904541a7e"
+
+Get-AzPolicyState -PolicyDefinitionName "ff25f3c8-b739-4538-9d07-3d6d25cfb255"
+
+Get-AzPolicyState -PolicyAssignmentName "7f89b1eb-583c-429a-8828-af049802c1d9"
+
+3cf2ab00-13f1-4d0c-8971-2ac904541a7e
+
+
+
+$tit = Get-AzPolicyAssignment -Name "4f11b553-d42e-4e3a-89be-32ca364cad4c"
+
+$tit.Properties.DisplayName
+# -Name "A maximum of 3 owners should be designated for your subscription"
+
+
+Get-AzPolicyDefinition -Name "A maximum of 3 owners should be designated for your subscription"
+
+Get-AzPolicyAssignment -PolicyDefinitionId 
+
+# work
+$tho = Get-AzPolicyState -PolicyAssignmentName "09024ccc-0c5f-475e-9457-b7c0d9ed487b"
+
+$tho.AdditionalProperties
+
+'A maximum of 3 owners should be designated for your subscription'`
+# "4f11b553-d42e-4e3a-89be-32ca364cad4c"
+
+$tho = Get-AzPolicyState -PolicyAssignmentName "A maximum of 3 owners should be designated for your subscription"
+
+
+
+#  ----------------------
+# Creating array to store values
+$Script:FinalArray = @()
+
+# File Path for CSV saved in variable "pls edit the path as you wish - 'WITH NO TRAILING \'
+# Example - "C:\Users\user\Desktop"   and not this - "C:\Users\user\Desktop\" 
+# '.\' means the present working directory (where the script is been ran) you can type in PWD in the Powershell to know the location if still unclear
+$Script:CSVFilePath = ".\"
+
+# File name for CSV saved in variable
+$Script:CSVFileName = "ISO27001-2013-Compliance-Check-" + (Get-Date -Format "MM-dd-yyyy-HH-mm") + ".csv"
+
+# Literal Path saved in variable
+$Script:LiteralPath = $Script:CSVFilePath + "\" + $Script:CSVFileName
+
+# Set the Azure Subscription where Policy Assignment is going to take place.
+$Script:Subscription = Get-AzSubscription -SubscriptionName 'Visual Studio Enterprise Subscription'
+
+$AzPolicyAssignments = Get-AzPolicyAssignment -Scope "/subscriptions/$($Script:Subscription.Id)"
+
+$AllComplianceStates = Get-AzPolicyState -Filter "ComplianceState eq 'NonCompliant'" -SubscriptionId $Script:Subscription.Id
+
+foreach ($AzPolicyAssignment in $AzPolicyAssignments) {
+    if (
+        #Region ISO 27001:2013 policy names from - https://docs.microsoft.com/en-us/azure/governance/policy/samples/iso-27001
+        $AzPolicyAssignment.Properties.DisplayName -match 'Virtual machines should encrypt temp disks, caches, and data flows between Compute and Storage resources'`
+            -or $AzPolicyAssignment.Properties.DisplayName -match 'There should be more than one owner assigned to your subscription'
+
+    ) {
+        $PolicyStates = $AllComplianceStates | Where-Object { $_.PolicyDefinitionName -match $AzPolicyAssignment.Name }
+
+        foreach ($PolicyState in $PolicyStates) {
+            # Save the Policy Information in Custom Object 
+            $Script:FinalArray += [PSCustomObject][Ordered]@{
+                "PolicyDisplayName"      = (($AzPolicyAssignment).Properties).DisplayName
+                "PolicyDefinitionId"     = ($PolicyState).PolicyDefinitionId
+                "ComplianceState"        = ($PolicyState).ComplianceState
+                "ResourceId"             = ($PolicyState).ResourceId
+                "SubscriptionId"         = ($PolicyState).SubscriptionId
+                "ResourceType"           = ($PolicyState).ResourceType
+                "ResourceLocation"       = ($PolicyState).ResourceLocation
+                "ResourceGroup"          = ($PolicyState).ResourceGroup
+                "ResourceTags"           = ($PolicyState).ResourceTags
+                "PolicyAssignmentName"   = ($PolicyState).PolicyAssignmentName
+                "PolicyAssignmentOwner"  = ($PolicyState).PolicyAssignmentOwner
+                "PolicyAssignmentScope"  = ($PolicyState).PolicyAssignmentScope
+                "PolicyDefinitionName"   = ($PolicyState).PolicyDefinitionName
+                "PolicyDefinitionAction" = ($PolicyState).PolicyDefinitionAction
+            }
+        }
+
+        
+    }
+    else {
+        # Do nothing
+    }
+}
+
+# Export to CSV
+$Script:FinalArray | Export-Csv -LiteralPath $Script:LiteralPath -NoTypeInformation -Force
+
+# Write Out the value to screen
+Write-Output $Script:FinalArray
+
+ATTUNE_PUSH_FILES (file://DESKTOP-G3SELD4/Users/user/Desktop/ATTUNE/ATTUNE_PUSH_FILES)
